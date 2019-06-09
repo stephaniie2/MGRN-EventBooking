9a2986ea4dd0b62492b36f388fd95c15f8b1a11b
Stephanie <stephaniiehallberg@gmail.com>
2019-06-09 14:25:11 +0200

    GraphQL + MongoDB

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+const Event = require('./models/event');
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
-// Root variables enable multiple queries and mutations. 
-app.use('/graphql', graphqlHttp({
-  schema: buildSchema(`
+// Root variables enable multiple queries and mutations.
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: buildSchema(`
 
     type Event {
       _id: ID!
@@ -41,23 +43,53 @@ app.use('/graphql', graphqlHttp({
       mutation: RootMutation
     }
   `),
-  rootValue: {
-    events: () => {
-      return events;
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+        });
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
     },
-    createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: args.eventInput.date
-      };
-      events.push(event);
-      return event;
-    }
-  },
-  graphiql: true
-}));
+    graphiql: true,
+  })
+);
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@eventbooking-cizwq.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
