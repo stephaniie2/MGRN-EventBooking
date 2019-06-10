df3c9deac7190da2d372ce8d694658c3c3e4461a
Stephanie <stephaniiehallberg@gmail.com>
2019-06-10 17:09:28 +0200

    React - Using Token and Context

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,27 +1,39 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import AuthContext from '../../context/auth-context';
+
 import './MainNavigation.css';
 
 const MainNavigation = props => (
-  <header className="main-navigation">
-    <div className="main-navigation__logo">
-      <h1>FunEvents</h1>
-    </div>
-    <nav className="main-navigation__items">
-      <ul>
-        <li>
-          <NavLink to="/auth">Authenticate</NavLink>
-        </li>
-        <li>
-          <NavLink to="/events">Events</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bookings">Bookings</NavLink>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <AuthContext.Consumer>
+    {context => {
+      return (
+        <nav className="main-navigation">
+          <div className="main-navigation__logo">
+            <h1>FunEvents</h1>
+          </div>
+          <nav className="main-navigation__items">
+            <ul>
+              {!context.token && (
+                <li>
+                  <NavLink to="/auth">Authenticate</NavLink>
+                </li>
+              )}
+              <li>
+                <NavLink to="/events">Events</NavLink>
+              </li>
+              {context.token && (
+                <li>
+                  <NavLink to="/bookings">Bookings</NavLink>
+                </li>
+              )}
+            </ul>
+          </nav>
+        </nav>
+      );
+    }}
+  </AuthContext.Consumer>
 );
 
 export default MainNavigation;
